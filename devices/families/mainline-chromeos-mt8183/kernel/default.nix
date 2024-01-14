20c19351c7258a0a8886d5b33d7bec305b4cb615
codgician <15964984+codgician@users.noreply.github.com>
2024-01-15 02:33:41 +0800

    revert patch

--- devices/families/mainline-chromeos-mt8183/kernel/default.nix
+++ devices/families/mainline-chromeos-mt8183/kernel/default.nix
@@ -14,7 +14,14 @@ mobile-nixos.kernel-builder {
     sha256 = "sha256-r+LlpmG7iG12JoTr6nFgfR7oy53RACedKBC6INlnHlI=";
   };
 
-  patches = [ ];
+  patches = [ 
+    # CHROMIUM: Revert "serial: 8250_mtk: Fix UART_EFR register address"
+    # https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3670640
+    (fetchpatch {
+      url = "https://github.com/torvalds/linux/commit/4cec85ca5a098fca3d49bda9976bccaca16a8876.patch";
+      sha256 = "sha256-V5d1OSJro82LIWrlJ74m5xxF26dtEe7HZmoFgUX/HBc=";
+    })
+  ];
 
   isModular = true;
   isCompressed = false;
